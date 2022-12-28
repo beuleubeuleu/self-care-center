dae69f456e7d555e4a5eb42284c158b1b21302e8
beuleu <billyklotz789@gmail.com>
2022-12-28 15:17:42 +0100

    CTPA issue gestion d'errreur done

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const form = document.getElementById("message-type")
 const mantra = document.getElementById("mantra")
 const affirmation = document.getElementById("affirmation")
 const displayWindow = document.querySelector(".display-window")
+const resetButton = document.querySelector(".reset")
 
 const affirmations = ["When faced with a problem, just say hisashiburi dana", "if the div won't center, just close the computer", "On passe notre vie à tout prendre, au final on va tout rendre"]
 const mantras = ["just act like you know", "🧎"]
@@ -14,26 +15,41 @@ function sleep(ms){
 }
 
 window.addEventListener("DOMContentLoaded", () => {
+
+    resetButton.addEventListener("click", () => {
+        const content = document.querySelector(".display-window :first-child")
+        content.classList.add("fade-out")
+        sleep(1000).then(() => {
+            content.parentNode.removeChild(content)
+            let meditateImage = document.createElement("img");
+            meditateImage.src = "assets/meditate.svg";
+            meditateImage.alt = "i am zen"
+            displayWindow.appendChild(meditateImage)
+        })
+    })
+
     form.addEventListener("submit", function(e) {
         e.preventDefault(); // before the code
         /* do what you want with the form */
-        const content = document.querySelector(".display-window :first-child")
         // Should be triggered on form submit
-        content.classList.add("fade-out")
-        sleep(1000).then(()=>{
-            content.parentNode.removeChild(content)
-            if (mantra.checked) {
-                let tag = document.createElement("p")
-                let node = document.createTextNode(`"${randomIndex(mantras)}"`)
-                tag.appendChild(node)
-                displayWindow.appendChild(tag)
-            }
-            if (affirmation.checked) {
-                let tag = document.createElement("p")
-                let node = document.createTextNode(`"${randomIndex(affirmations)}"`)
-                tag.appendChild(node)
-                displayWindow.appendChild(tag)
-            }
-        })
+        if (mantra.checked || affirmation.checked) {
+            const content = document.querySelector(".display-window :first-child")
+            content.classList.add("fade-out")
+            sleep(1000).then(() => {
+                content.parentNode.removeChild(content)
+                if (mantra.checked) {
+                    let tag = document.createElement("p")
+                    let node = document.createTextNode(`"${randomIndex(mantras)}"`)
+                    tag.appendChild(node)
+                    displayWindow.appendChild(tag)
+                }
+                if (affirmation.checked) {
+                    let tag = document.createElement("p")
+                    let node = document.createTextNode(`"${randomIndex(affirmations)}"`)
+                    tag.appendChild(node)
+                    displayWindow.appendChild(tag)
+                }
+            })
+        }
     })
 });
