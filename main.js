1361fb8d5e9da5b6f7fe8e1f95fe096a0302453f
beuleu <billyklotz789@gmail.com>
2023-01-10 14:38:17 +0100

    add one affirmations

--- main.js
+++ main.js
@@ -12,7 +12,8 @@ const affirmations = ["When faced with a problem, just say hisashiburi dana",
     "if the div won't center, just close the computer",
     "On passe notre vie à tout prendre, au final on va tout rendre",
     "A life without pain, Is a life without growth",
-    "La nuit porte conseil que quand tu dors"]
+    "La nuit porte conseil que quand tu dors",
+    "stand tall then i can't fall"]
 const mantras = ["just act like you know",
                 "take a step forward",
                 "pourquoi t'as commencé",
